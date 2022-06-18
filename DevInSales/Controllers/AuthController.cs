312ead0db7d3b740413df14ab698b2036a9f8241
rmyght <ramon.stelles@gmail.com>
2022-06-17 22:15:07 -0300

    Criada a solicitação para gerar um Token através do login (Post - ApiLogin).

---
+++ DevInSales/Controllers/AuthController.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using DevInSales.Context;
+using DevInSales.DTOs;
+using DevInSales.Models;
+using DevInSales.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevInSales.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly SqlContext _context;
+
+        public AuthController(SqlContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        [Route("login")]
+        public IActionResult ApiLogin([FromBody] LoginDTO dto)
+        {
+            var userLogin = _context.User.FirstOrDefault(l => l.Email == dto.Email && l.Password == dto.Password);
+
+            if (userLogin == null)
+                return NotFound("Usuário não existe ou a senha está incorreta!");
+
+            var token = TokenServices.GenerateToken(userLogin);
+            return Ok(token);
+        }
+    }
+}
---
+++ DevInSales/DTOs/LoginDTO.cs
@@ -0,0 +1,10 @@
+﻿using DevInSales.Models;
+
+namespace DevInSales.DTOs
+{
+    public class LoginDTO
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
